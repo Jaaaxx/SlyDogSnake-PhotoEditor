b4f3f90665f95ab2c53deef9d4281873d7a7ea62
BuildTools <unconfigured@null.spigotmc.org>
2021-10-10 23:30:18 -0400

    not working :(

--- SlyDogSnake/main.c
+++ SlyDogSnake/main.c
@@ -2,6 +2,14 @@
 #include <stdint.h>
 #include <SDL.h>
 
+Uint32 get_pixel32(SDL_Surface* surface, int x, int y) {
+	//Convert the pixels to 32 bit
+	Uint32* pixels = (Uint32*)surface->pixels;
+
+	//Get the requested pixel
+	return pixels[(y * surface->w) + x];
+}
+
 int main(int argc, char** argv) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 		printf("Error initializing SDL:%s\n", SDL_GetError());
@@ -13,25 +21,25 @@ int main(int argc, char** argv) {
 		printf("Error creating window:%s\n", SDL_GetError());
 		return 1;
 	}
-
 	SDL_Surface* screen = SDL_GetWindowSurface(window);
 	SDL_Surface* pImg = SDL_LoadBMP("Resources/stick_man.bmp");
+	SDL_UpdateWindowSurface(window);
 	for (int x = 0; x < pImg->w; x++) {
 		for (int y = 0; y < pImg->h; y++) {
-			SDL_LockSurface(pImg);
-			uint32_t pixel = (uint32_t) (uint8_t) pImg->pixels + y * pImg->pitch + x * pImg->format->BytesPerPixel;
 			uint8_t rgb[3];
-			SDL_GetRGB(pixel, pImg->format, &rgb[0], &rgb[1], &rgb[2]);
-
-			if (rgb[0] >= 10) {
-				SDL_UnlockSurface(pImg);
-				SDL_FillRect(pImg, &(SDL_Rect) { x, y, 1, 1 }, SDL_MapRGB(pImg->format, 10, 155, 155));
-			}
+			SDL_GetRGB(get_pixel32(pImg,x,y), pImg->format, &rgb[0], &rgb[1], &rgb[2]);
+			printf("%d/%dw | %d/%dh\n", x, pImg->w, y, pImg->h);
+			if (rgb[2] >= 100)
+				SDL_FillRect(pImg, &(SDL_Rect) { x, y, 1, 1 }, SDL_MapRGB(pImg->format, 255, 0, 0));
 		}
 	}
-
+	SDL_SetWindowSize(window, pImg->w, pImg->h);
+	screen = SDL_GetWindowSurface(window);
+	SDL_BlitSurface(pImg, NULL, screen, &(SDL_Rect){ 0,0 });
+	SDL_UpdateWindowSurface(window);
 	SDL_SaveBMP(pImg, "saved.bmp");
 
+	while (1);
 	SDL_DestroyWindow(window);
 
 	SDL_Quit();
